f7b8832ef101ba77fcffc49e85570724415535a1
Gabriele Gentili <gento84@gmail.com>
2025-02-24 15:28:37 +0100

    a

--- js/cerca.js
+++ js/cerca.js
@@ -18,3 +18,25 @@ function searchBooks() {
       }
   }
 }
+
+
+function searchIdee() {
+    var input, filter, ul, li, strong, em, i, txtValue, authorValue;
+    input = document.getElementById('search-input');
+    filter = input.value.toUpperCase().trim(); // Rimuove spazi bianchi e converte in maiuscolo
+    ul = document.getElementById('idee');
+    li = ul.getElementsByTagName('li');
+  
+    for (i = 0; i < li.length; i++) {
+        strong = li[i].getElementsByTagName('strong')[0];
+        txtValue = strong.textContent || strong.innerText;
+        authorValue = em.textContent || em.innerText;
+        
+        if (txtValue.toUpperCase().indexOf(filter) > -1 || authorValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+  }
+  
