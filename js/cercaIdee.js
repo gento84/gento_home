b9f1e07408021576079a51bd55d636876058a08a
Gabriele Gentili <gento84@gmail.com>
2025-02-24 15:56:48 +0100

    s

--- js/cercaIdee.js
+++ js/cercaIdee.js
@@ -1,14 +1,11 @@
 function searchIdee() {
     // Ottieni il valore dell'input di ricerca
     let input = document.getElementById('search-input').value.toLowerCase();
-    
     // Ottieni tutti gli elementi della lista
     let items = document.querySelectorAll('.idee li');
-    
     items.forEach(item => {
       // Ottieni il titolo dell'elemento (testo dell'elemento <a>)
       let title = item.querySelector('strong a').textContent.toLowerCase();
-      
       // Controlla se il titolo include il valore dell'input di ricerca
       if (title.includes(input)) {
         // Mostra l'elemento se corrisponde alla ricerca
