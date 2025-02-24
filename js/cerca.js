c51d5cea906d50ba3790eba7ac973ddbda929b91
Gabriele Gentili <gento84@gmail.com>
2025-02-24 08:53:53 +0100

    js

--- js/cerca.js
+++ js/cerca.js
@@ -1,5 +1,5 @@
 function searchBooks() {
-    var input, filter, ul, li, strong, i, txtValue;
+    var input, filter, ul, li, strong, i, txtValue, authorValue;
     input = document.getElementById('search-input');
     filter = input.value.toUpperCase().trim(); // Rimuove spazi bianchi e converte in maiuscolo
     ul = document.getElementById('libri-list');
@@ -7,7 +7,9 @@ function searchBooks() {
   
     for (i = 0; i < li.length; i++) {
       strong = li[i].getElementsByTagName('strong')[0];
+      em = li[i].getElementsByTagName('em')[0];
       txtValue = strong.textContent || strong.innerText;
+      authorValue = em.textContent || em.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
         li[i].style.display = "";
       } else {
