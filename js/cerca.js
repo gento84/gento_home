49f7b9a66bf9e242d9a003bcd6e6839524bb5fc4
Gabriele Gentili <gento84@gmail.com>
2025-02-24 09:01:44 +0100

    book4

--- js/cerca.js
+++ js/cerca.js
@@ -1,20 +1,20 @@
 function searchBooks() {
-    var input, filter, ul, li, strong, i, txtValue, authorValue;
-    input = document.getElementById('search-input');
-    filter = input.value.toUpperCase().trim(); // Rimuove spazi bianchi e converte in maiuscolo
-    ul = document.getElementById('libri-list');
-    li = ul.getElementsByTagName('li');
-  
-    for (i = 0; i < li.length; i++) {
+  var input, filter, ul, li, strong, em, i, txtValue, authorValue;
+  input = document.getElementById('search-input');
+  filter = input.value.toUpperCase().trim(); // Rimuove spazi bianchi e converte in maiuscolo
+  ul = document.getElementById('libri-list');
+  li = ul.getElementsByTagName('li');
+
+  for (i = 0; i < li.length; i++) {
       strong = li[i].getElementsByTagName('strong')[0];
       em = li[i].getElementsByTagName('em')[0];
       txtValue = strong.textContent || strong.innerText;
       authorValue = em.textContent || em.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "";
+      
+      if (txtValue.toUpperCase().indexOf(filter) > -1 || authorValue.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = "";
       } else {
-        li[i].style.display = "none";
+          li[i].style.display = "none";
       }
-    }
   }
-  
+}
