ebadb2fa734e0dc7abb0000bc5a6b650f551cb4e
Gabriele Gentili <gento84@gmail.com>
2025-02-24 09:53:39 +0100

    popup

---
+++ js/popup.js
@@ -0,0 +1,11 @@
+function openModal(img) {
+    var modal = document.getElementById("myModal");
+    var modalImg = document.getElementById("img01");
+
+    modal.style.display = "block";
+    modalImg.src = img.src;
+}
+
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
